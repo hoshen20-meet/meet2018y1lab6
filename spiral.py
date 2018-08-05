68e70de443eba980b1de8b865eea8337aa82e6d3
hoshen20-meet <hoshen20@meet.mit.edu>
2018-08-05 09:28:07 -0400

    Stopping here

---
+++ spiral.py
@@ -0,0 +1,17 @@
+import turtle
+
+colors = ['green','blue','orange', 'red']
+
+
+turtle.speed(900)
+for i in range(99999999):
+    turtle.pencolor(colors[i%4])
+    turtle.bgcolor('black')
+    turtle.forward(i)
+    turtle.degrees()
+
+    turtle.right(421)
+    
+    
+    
+
